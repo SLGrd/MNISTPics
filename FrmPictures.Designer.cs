c738c094743e8c91fde6df3e5d4004f9b60c0867
Sylvio Leal Giraldes <sylviogiraldes@yahoo.com>
2023-03-03 00:08:10 +0000

    Add project files.

---
+++ FrmPictures.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace MNISTPics
+{
+    partial class FrmPictures
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            picB = new PictureBox();
+            labNumber = new Label();
+            btnLoad = new Button();
+            btnGetNext = new Button();
+            btnGetPrevious = new Button();
+            label1 = new Label();
+            txtCurrentRow = new TextBox();
+            label2 = new Label();
+            txtRowCount = new TextBox();
+            btnGetFirst = new Button();
+            btnGetLast = new Button();
+            chkDarkMode = new CheckBox();
+            txtFileLocation = new TextBox();
+            label3 = new Label();
+            ((System.ComponentModel.ISupportInitialize)picB).BeginInit();
+            SuspendLayout();
+            // 
+            // picB
+            // 
+            picB.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            picB.BorderStyle = BorderStyle.FixedSingle;
+            picB.Location = new Point(94, 152);
+            picB.Name = "picB";
+            picB.Size = new Size(618, 541);
+            picB.TabIndex = 0;
+            picB.TabStop = false;
+            // 
+            // labNumber
+            // 
+            labNumber.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            labNumber.AutoSize = true;
+            labNumber.Font = new Font("Segoe UI", 16F, FontStyle.Regular, GraphicsUnit.Point);
+            labNumber.Location = new Point(385, 104);
+            labNumber.Name = "labNumber";
+            labNumber.Size = new Size(34, 45);
+            labNumber.TabIndex = 1;
+            labNumber.Text = "?";
+            // 
+            // btnLoad
+            // 
+            btnLoad.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            btnLoad.FlatStyle = FlatStyle.Popup;
+            btnLoad.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            btnLoad.Location = new Point(769, 367);
+            btnLoad.Name = "btnLoad";
+            btnLoad.Size = new Size(220, 45);
+            btnLoad.TabIndex = 2;
+            btnLoad.Text = "Load Data";
+            btnLoad.UseVisualStyleBackColor = true;
+            btnLoad.Click += BtnLoad_Click;
+            // 
+            // btnGetNext
+            // 
+            btnGetNext.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            btnGetNext.FlatStyle = FlatStyle.Popup;
+            btnGetNext.Location = new Point(463, 721);
+            btnGetNext.Name = "btnGetNext";
+            btnGetNext.Size = new Size(117, 34);
+            btnGetNext.TabIndex = 2;
+            btnGetNext.Text = ">>";
+            btnGetNext.UseVisualStyleBackColor = true;
+            btnGetNext.Click += BtnGetNext_Click;
+            // 
+            // btnGetPrevious
+            // 
+            btnGetPrevious.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            btnGetPrevious.FlatStyle = FlatStyle.Popup;
+            btnGetPrevious.Location = new Point(227, 721);
+            btnGetPrevious.Name = "btnGetPrevious";
+            btnGetPrevious.Size = new Size(117, 34);
+            btnGetPrevious.TabIndex = 2;
+            btnGetPrevious.Text = "<<";
+            btnGetPrevious.UseVisualStyleBackColor = true;
+            btnGetPrevious.Click += BtnGetPrevious_Click;
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            label1.AutoSize = true;
+            label1.ForeColor = Color.DarkRed;
+            label1.Location = new Point(769, 182);
+            label1.Name = "label1";
+            label1.Size = new Size(132, 25);
+            label1.TabIndex = 3;
+            label1.Text = "Current Row #";
+            // 
+            // txtCurrentRow
+            // 
+            txtCurrentRow.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            txtCurrentRow.BorderStyle = BorderStyle.FixedSingle;
+            txtCurrentRow.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            txtCurrentRow.Location = new Point(769, 210);
+            txtCurrentRow.Name = "txtCurrentRow";
+            txtCurrentRow.Size = new Size(220, 39);
+            txtCurrentRow.TabIndex = 4;
+            txtCurrentRow.TextAlign = HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            label2.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            label2.AutoSize = true;
+            label2.ForeColor = Color.DarkRed;
+            label2.Location = new Point(769, 278);
+            label2.Name = "label2";
+            label2.Size = new Size(103, 25);
+            label2.TabIndex = 3;
+            label2.Text = "Row Count";
+            // 
+            // txtRowCount
+            // 
+            txtRowCount.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            txtRowCount.BorderStyle = BorderStyle.FixedSingle;
+            txtRowCount.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            txtRowCount.Location = new Point(769, 306);
+            txtRowCount.Name = "txtRowCount";
+            txtRowCount.Size = new Size(220, 39);
+            txtRowCount.TabIndex = 4;
+            txtRowCount.TextAlign = HorizontalAlignment.Center;
+            // 
+            // btnGetFirst
+            // 
+            btnGetFirst.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            btnGetFirst.FlatStyle = FlatStyle.Popup;
+            btnGetFirst.Location = new Point(94, 721);
+            btnGetFirst.Name = "btnGetFirst";
+            btnGetFirst.Size = new Size(117, 34);
+            btnGetFirst.TabIndex = 2;
+            btnGetFirst.Text = "<<<<";
+            btnGetFirst.UseVisualStyleBackColor = true;
+            btnGetFirst.Click += BtnGetFirst_Click;
+            // 
+            // btnGetLast
+            // 
+            btnGetLast.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            btnGetLast.FlatStyle = FlatStyle.Popup;
+            btnGetLast.Location = new Point(595, 721);
+            btnGetLast.Name = "btnGetLast";
+            btnGetLast.Size = new Size(117, 34);
+            btnGetLast.TabIndex = 2;
+            btnGetLast.Text = ">>>>";
+            btnGetLast.UseVisualStyleBackColor = true;
+            btnGetLast.Click += BtnGetLast_Click;
+            // 
+            // chkDarkMode
+            // 
+            chkDarkMode.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            chkDarkMode.CheckAlign = ContentAlignment.BottomRight;
+            chkDarkMode.Checked = true;
+            chkDarkMode.CheckState = CheckState.Checked;
+            chkDarkMode.FlatStyle = FlatStyle.System;
+            chkDarkMode.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            chkDarkMode.Location = new Point(769, 457);
+            chkDarkMode.Name = "chkDarkMode";
+            chkDarkMode.Size = new Size(220, 44);
+            chkDarkMode.TabIndex = 5;
+            chkDarkMode.Text = "Dark Mode";
+            chkDarkMode.UseVisualStyleBackColor = true;
+            chkDarkMode.CheckedChanged += ChkDarkMode_CheckedChanged;
+            // 
+            // txtFileLocation
+            // 
+            txtFileLocation.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            txtFileLocation.BorderStyle = BorderStyle.FixedSingle;
+            txtFileLocation.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            txtFileLocation.Location = new Point(94, 46);
+            txtFileLocation.Name = "txtFileLocation";
+            txtFileLocation.Size = new Size(618, 39);
+            txtFileLocation.TabIndex = 6;
+            txtFileLocation.TextAlign = HorizontalAlignment.Center;
+            txtFileLocation.Leave += TxtFileLocation_Leave;
+            // 
+            // label3
+            // 
+            label3.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            label3.AutoSize = true;
+            label3.ForeColor = Color.DarkRed;
+            label3.Location = new Point(94, 18);
+            label3.Name = "label3";
+            label3.Size = new Size(118, 25);
+            label3.TabIndex = 7;
+            label3.Text = "File Location";
+            // 
+            // FrmPictures
+            // 
+            AutoScaleDimensions = new SizeF(11F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1024, 783);
+            Controls.Add(label3);
+            Controls.Add(txtFileLocation);
+            Controls.Add(chkDarkMode);
+            Controls.Add(txtRowCount);
+            Controls.Add(label2);
+            Controls.Add(txtCurrentRow);
+            Controls.Add(label1);
+            Controls.Add(btnGetLast);
+            Controls.Add(btnGetNext);
+            Controls.Add(btnGetFirst);
+            Controls.Add(btnGetPrevious);
+            Controls.Add(btnLoad);
+            Controls.Add(labNumber);
+            Controls.Add(picB);
+            Name = "FrmPictures";
+            Text = "MNIST Pics Reader";
+            Activated += FrmPictures_Activated;
+            ((System.ComponentModel.ISupportInitialize)picB).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox picB;
+        private Label labNumber;
+        private Button btnLoad;
+        private Button btnGetNext;
+        private Button btnGetPrevious;
+        private Label label1;
+        private TextBox txtCurrentRow;
+        private Label label2;
+        private TextBox txtRowCount;
+        private Button btnGetFirst;
+        private Button btnGetLast;
+        private CheckBox chkDarkMode;
+        private TextBox txtFileLocation;
+        private Label label3;
+    }
+}
---
+++ FrmPictures.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System.Globalization;
+
+namespace MNISTPics;
+public partial class FrmPictures : Form
+{
+    #region Global Variables                    
+    int bookMark = 0, numberOfRows;         // Used to browse the results
+    readonly int nRows = 28, nCols = 28;    // Every digit is represented in a 28x28 pixels image
+    string[][]? data;                       // Full file will contain [60000][785]
+    // Hard coded file location
+    string path = @"D:\Tutorials\MNIST\HandWrtDigits\mnist_train.csv";
+    #endregion
+
+    public FrmPictures() => InitializeComponent();
+    private void FrmPictures_Activated(object sender, EventArgs e)
+    {
+        txtFileLocation.Text = path; txtFileLocation.Focus();
+    }
+    private void BtnLoad_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            Cursor.Current = Cursors.WaitCursor;
+
+            //  Begin ---- Here starts the code to load the database ----
+            int numberOfColumns = File.ReadLines(path).First().Split(',').Length;    // This number will be 784
+            numberOfRows = int.Parse(txtRowCount.Text, NumberStyles.AllowThousands); // Choose part of the data  
+
+            TextFieldParser parser = new TextFieldParser(path);
+            parser.TextFieldType = FieldType.Delimited;
+            parser.SetDelimiters(",");
+
+            data = new string[numberOfRows][];
+            for (int i = 0; i < numberOfRows; i++) { data[i] = parser.ReadFields()!; }
+            //  End   ---- Here ends the code to load the database  ----
+
+            //  Sort data array by digit 0 to 9
+            data = data.OrderBy(subarr => subarr[0]).ToArray();
+
+            //  Show first digit
+            bookMark = 0;
+            DrawDigit(bookMark);
+        }
+        catch (Exception ex)
+        { MessageBox.Show(ex.Message, this.Text, MessageBoxButtons.OK, MessageBoxIcon.Warning); }
+        finally { Cursor.Current = Cursors.Default; }
+    }
+    private void ChkDarkMode_CheckedChanged(object sender, EventArgs e)
+    {
+        if (data is not null) DrawDigit(bookMark);
+    }
+    private void BtnGetFirst_Click(object sender, EventArgs e)
+    {
+        bookMark = 0;
+        DrawDigit(bookMark);
+    }
+    private void BtnGetPrevious_Click(object sender, EventArgs e)
+    {
+        if (bookMark > 0) DrawDigit(--bookMark);
+    }
+    private void BtnGetNext_Click(object sender, EventArgs e)
+    {
+        if (bookMark < numberOfRows - 1) DrawDigit(++bookMark);
+    }
+    private void BtnGetLast_Click(object sender, EventArgs e)
+    {
+        bookMark = numberOfRows - 1;
+        DrawDigit(bookMark);
+    }
+    private void DrawDigit(int bkMark)
+    {
+        try
+        {
+            Bitmap image = new Bitmap(nCols, nRows); // Remember every digit was built as 28x28 pixels
+            int pixelValue = 0;
+
+            (string label, string[] pixels) Digit = (data![bkMark][0], data[bkMark][1..]);
+
+            labNumber.Text = Digit.label;
+
+            for (int row = 0; row < nRows; row++)
+                for (int column = 0; column < nCols; column++)
+                {
+                    pixelValue = int.Parse(Digit.pixels[row * nRows + column].ToString());
+                    pixelValue = (chkDarkMode.Checked ? pixelValue : 255 - pixelValue);     // Toggles dark mode
+                    image.SetPixel(column, row, Color.FromArgb(pixelValue, pixelValue, pixelValue));
+                }
+
+            picB.Image = image;
+            picB.SizeMode = PictureBoxSizeMode.Zoom;
+
+            txtCurrentRow.Text = bkMark.ToString("N0");
+        }
+        catch (Exception ex)
+        { MessageBox.Show(ex.Message, this.Text, MessageBoxButtons.OK, MessageBoxIcon.Warning); }
+    }
+    private void TxtFileLocation_Leave(object sender, EventArgs e)
+    {
+        CheckFileLocation();
+    }
+    private void CheckFileLocation()
+    {
+        try
+        {
+            path = txtFileLocation.Text;
+            txtRowCount.Text = File.ReadLines(path).Count().ToString("N0");
+        }
+        catch (IOException ex)
+        { MessageBox.Show(ex.Message, this.Text, MessageBoxButtons.OK, MessageBoxIcon.Warning); }
+    }
+}
